7c121c5e63e4c3bdec7fa6d38dbdffc14a885812
philipyoo <philipyoo10@gmail.com>
2016-10-12 20:03:15 +0000

    Edit solution for llp-4, indentation and formatting

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main(void) {
-    puts("Programming is like building a multilingual puzzle");
-    return 0;
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }

93a26916747c550250554aa42a955d00c359466f
philipyoo <philipyoo10@gmail.com>
2016-10-13 17:39:27 +0000

    Add solution for 0x01c-5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print all numbers of base 10, starting from 0.
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	putchar('\n');
+
+	return (0);
+}

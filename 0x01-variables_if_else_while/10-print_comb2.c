48fabeb587f222544122acce14a6d364183c8d73
philipyoo <philipyoo10@gmail.com>
2016-10-13 18:14:53 +0000

    Add solution for 0x01c-10

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print numbers from 0 to 99.
+ * Numbers must be separated by commas followed by a space.
+ * You can only use `putchar` to print to console.
+ * You can only use `putchar` up to 5 times.
+ * You are not allowed to use variables of type `char`.
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 100; i++)
+	{
+		if (i > 9)
+			putchar((i / 10) + '0');
+
+		putchar((i % 10) + '0');
+
+		if (i < 99)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}

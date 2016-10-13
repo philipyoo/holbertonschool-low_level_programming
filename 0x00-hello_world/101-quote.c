ec9e36bd57e960ebb132df0476e51d977dde6189
philipyoo <philipyoo10@gmail.com>
2016-10-13 00:06:57 +0000

    Edit solution for llp-9

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,10 @@
  */
 int main(void)
 {
+	int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
 
-  write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n"));
-
-  return (1);
+	write(2,
+	      "and that piece of art is useful - Dora Korpar, 2015-10-19\n",
+	      s);
+	return (1);
 }

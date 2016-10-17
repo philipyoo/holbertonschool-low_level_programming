c9020e3bba3787055f1239d5aa0608bf62043543
philipyoo <philipyoo10@gmail.com>
2016-10-17 21:32:02 +0000

    Add solution for 0x02c-2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * print_alphabet_x10 - Print alphabet in lowercase 10 times
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char c;
+
+	while (i < 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}

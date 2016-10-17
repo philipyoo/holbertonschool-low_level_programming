95b4c56d55b9b283235d1484d73c440fc74517dc
philipyoo <philipyoo10@gmail.com>
2016-10-17 21:16:48 +0000

    Add solution for 0x02c-1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * print_alphabet - Print alphabet in lowercase using only `_putchar`
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,2 +1,3 @@
 int _putchar(char c);
+void print_alphabet(void);
 

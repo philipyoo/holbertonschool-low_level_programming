516ec443bb955a96255372d0b74f36ae3829041b
philipyoo <philipyoo10@gmail.com>
2016-10-18 17:37:26 +0000

    Add solution for 0x02c-11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_to_98 - Print all natural numbers from n to 98
+ * @n: int type number
+ */
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n--;
+		}
+	}
+	else
+	{
+		while (n <= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++;
+		}
+	}
+	printf("\n");
+}

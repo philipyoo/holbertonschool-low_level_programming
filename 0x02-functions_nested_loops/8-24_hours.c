c0d64907efe190fdeabee8e8d20f57d51c94c26d
philipyoo <philipyoo10@gmail.com>
2016-10-18 00:36:08 +0000

    Add solution for 0x02c-8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+ * jack_bauer - Print every minute of the day of Jack Bauer
+ * Description: Start from 00:00 to 23:59
+ */
+void jack_bauer(void)
+{
+	int x = 0;
+	int a = 0, b = 0, c = 0, d = 0;
+	while (x < 1440)
+	{
+		_putchar(a + '0');
+		_putchar(b + '0');
+		_putchar(':');
+		_putchar(c + '0');
+		_putchar(d + '0');
+		_putchar('\n');
+
+		d++;
+		if (d > 9)
+		{
+			d = 0;
+			c++;
+		}
+		if (c > 5)
+		{
+			c = 0;
+			b++;
+		}
+		if (b > 9)
+		{
+			b = 0;
+			a++;
+		}
+		x++;
+	}
+}

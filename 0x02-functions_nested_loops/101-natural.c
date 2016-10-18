f7eb03768930e83a3fa2527a706610182d70137f
philipyoo <philipyoo10@gmail.com>
2016-10-18 19:13:24 +0000

    Add solution for 0x02c-13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - main block
+ * Description: Print the sum of all multiples of 3 or 5 below 1024.
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 1;
+	int total = 0;
+
+	while (i < 1024)
+	{
+		if (i % 3 == 0)
+			total += i;
+		else if (i % 5 == 0)
+			total += i;
+
+		i++;
+	}
+	printf("%d\n", total);
+
+	return (0);
+}

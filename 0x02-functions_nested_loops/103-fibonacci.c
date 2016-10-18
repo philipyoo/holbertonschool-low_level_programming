802b9e8ea1d2bdb4e10dff8281b463a34c97a011
philipyoo <philipyoo10@gmail.com>
2016-10-18 19:46:10 +0000

    Edit solution for 0x02c-15, only sum even numbers

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -2,18 +2,20 @@
 
 /**
  * main - main block
- * Description: Print the sum of Fibonacci numbers up to a fib value
+ * Description: Print the sum of even Fibonacci numbers up to a fib value
  * not exceeding 4,000,000.
  * Return: 0
  */
 int main(void)
 {
-	int i = 1, j = 2, total = i;
+	int i = 1, j = 2, total = 0;
 	int k;
 
 	while (j < 4000000)
 	{
-		total += j;
+		if (j % 2 == 0)
+			total += j;
+
 		k = j;
 		j += i;
 		i = k;

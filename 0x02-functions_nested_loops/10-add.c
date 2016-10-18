b5095f06025bd5503cdb39535cab582feb8c30ec
philipyoo <philipyoo10@gmail.com>
2016-10-18 17:27:20 +0000

    Add solution for 0x02c-10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * add - Add two integers
+ * @x: int type number
+ * @y: int type number
+ * Return: result of addition
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}

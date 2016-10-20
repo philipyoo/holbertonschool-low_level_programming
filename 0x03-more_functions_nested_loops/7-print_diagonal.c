cc16f1c3ad617d9ab673b571fb205f65659abafd
philipyoo <philipyoo10@gmail.com>
2016-10-20 17:27:53 +0000

    Add solution for 0x03c-7

---
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - draw a diagonal line
+ * @n: number of times the '\' char is printed
+ * Description: Can only use _putchar to print
+ */
+void print_diagonal(int n)
+{
+	int c, i;
+
+	c = 0;
+	while (n > 0)
+	{
+		i = c;
+		while (i > 0)
+		{
+			_putchar(' ');
+			i--;
+		}
+		_putchar('\\');
+		_putchar('\n');
+		c++;
+		n--;
+	}
+
+	if (c > 0)
+		_putchar('\n');
+
+}

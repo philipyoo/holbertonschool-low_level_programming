ba53771a8092b9da8da90d8369a4e115bfe63c94
philipyoo <philipyoo10@gmail.com>
2016-10-20 16:57:14 +0000

    Add solution for 0x03c-1

---
+++ 0x03-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _isdigit - Check for digits (0 through 9)
+ * @c: int type param
+ * Return: 1 if digit, else 0
+ */
+int _isdigit(int c)
+{
+	if (c > 47 && c < 58)
+		return (1);
+
+	return (0);
+}

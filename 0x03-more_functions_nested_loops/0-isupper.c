261699c7a223460150ea60c38bc9965561130ef7
philipyoo <philipyoo10@gmail.com>
2016-10-20 16:53:47 +0000

    Add solution for 0x03c-0, add _putchar function

---
+++ 0x03-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _isupper - function that checks for uppercase characters
+ * @c: int type number
+ * Return: 1 if uppercase, else 0
+ */
+int _isupper(int c)
+{
+	if (c > 64 && c < 91)
+		return (1);
+
+	return (0);
+}

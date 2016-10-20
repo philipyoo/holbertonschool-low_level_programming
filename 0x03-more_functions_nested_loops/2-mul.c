3c502ecd963389060c3ad56dc27024cdd92ed4ca
philipyoo <philipyoo10@gmail.com>
2016-10-20 17:00:01 +0000

    Add solution for 0x03c-2

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * mul - multiply 2 integers
+ * @a: int type number
+ * @b: int type number
+ * Return: result of multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}

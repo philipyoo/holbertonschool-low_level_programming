b78eb284ecae4562a27c872a5f20835b13bc7542
philipyoo <philipyoo10@gmail.com>
2016-10-20 17:03:51 +0000

    Add solution for 0x03c-3

---
+++ 0x03-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - print the numbers from 0 to 9
+ * Description: You can only use _putchar twice
+ */
+void print_numbers(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		_putchar(i + '0');
+		i++;
+	}
+	_putchar('\n');
+}

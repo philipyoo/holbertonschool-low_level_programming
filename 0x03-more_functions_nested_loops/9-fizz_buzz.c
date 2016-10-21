bcd9032252256e175b6bea3fad5b5af69514a4ec
philipyoo <philipyoo10@gmail.com>
2016-10-21 00:29:00 +0000

    Edit solution for 0x03c-9, remove last space

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,14 +14,16 @@ int main(void)
 	while (i <= 100)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else if (i % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else if (i % 5 == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else
-			printf("%d ", i);
+			printf("%d", i);
 
+		if (i != 100)
+			printf(" ");
 		i++;
 	}
 	printf("\n");

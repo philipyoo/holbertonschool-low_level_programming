4c8ca42658d82b61850d723444f7386251047e2a
philipyoo <philipyoo10@gmail.com>
2016-10-25 23:02:42 +0000

    Edit documentation for 0x04c-10

--- 0x04-pointers_arrays_strings/100-atoi.c
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -8,8 +8,6 @@
  * You need to take into account all -/+ signs before the number.
  * If there are no numbers in the string, return 0.
  * No need to check for overflow.
- * Not allowed to use `long` type.
- * Not allowed to declare new variables of "type" array.
  * Not allowed to hard-code special values.
  * Return: first integer found in string
  */

7d3732ee20764b2a0adecb1ee27eea3642115d2a
philipyoo <philipyoo10@gmail.com>
2016-10-25 23:11:46 +0000

    Edit solution for 0x04c-10, account for largest and smallest int

--- 0x04-pointers_arrays_strings/100-atoi.c
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -18,7 +18,7 @@ int _atoi(char *s)
 	int h, p;
 
 	h = 0;
-	p = 1;
+	p = -1;
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == '-')
@@ -26,10 +26,10 @@ int _atoi(char *s)
 
 		if (s[i] > 47 && s[i] < 58)
 		{
-			if (h > 0)
-				h = (h * 10) + (s[i] - '0');
+			if (h < 0)
+				h = (h * 10) - (s[i] - '0');
 			else
-				h = (s[i] - '0');
+				h = (s[i] - '0') * -1;
 
 			if (s[i + 1] < 48 || s[i + 1] > 57)
 				break;

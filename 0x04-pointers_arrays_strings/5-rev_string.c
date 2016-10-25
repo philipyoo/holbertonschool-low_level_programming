703553cb8d7f1b2cb9bb8feb909137489cf2fca8
philipyoo <philipyoo10@gmail.com>
2016-10-25 18:15:40 +0000

    Edit solution for 0x04c-5

--- 0x04-pointers_arrays_strings/5-rev_string.c
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -13,8 +13,8 @@ void rev_string(char *s)
 	for (i = 0; s[i] != '\0'; i++)
 		;
 
-	l = --i;
-	for (c = 0; c < l / 2; i--, c++)
+	l = i;
+	for (i--, c = 0; c < l / 2; i--, c++)
 	{
 		h = s[c];
 		s[c] = s[i];

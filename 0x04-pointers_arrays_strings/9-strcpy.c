98c1b58962c121cd0d24975fef86598606792a25
philipyoo <philipyoo10@gmail.com>
2016-10-25 19:28:40 +0000

    Add solution for 0x04c-9

---
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * *_strcpy - See description
+ * @dest: char type string
+ * @src: char type string
+ * Description: Copy the string pointed to by pointer `src` to
+ * the buffer pointed to by `dest`
+ * Return: Pointer to `dest`
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}

3f5d503914f8af6688e71f3d78a4d7f378d4d567
philipyoo <philipyoo10@gmail.com>
2016-10-25 16:47:30 +0000

    Add solution for 0x04c-2

---
+++ 0x04-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _strlen - return the length of the string
+ * @s: char type pointer
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int c;
+
+	for (c = 0; s[c] != '\0'; c++)
+		;
+
+	return (c);
+}

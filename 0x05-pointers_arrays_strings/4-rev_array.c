bfb699bf817fcddfbf74b77fe09c33dd6a064745
philipyoo <philipyoo10@gmail.com>
2016-10-27 21:17:49 +0000

    Add solution for 0x05c-4

---
+++ 0x05-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - reverse the contents of an array of integers
+ * @a: array of integers
+ * @n: number of elements in array
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i;
+	int h;
+
+	for (i = 0; i < (n / 2); i++)
+	{
+		h = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = h;
+	}
+}

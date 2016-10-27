a291bf93ea7bb3e07a83adcabb7cff1af416e9c0
philipyoo <philipyoo10@gmail.com>
2016-10-27 20:34:48 +0000

    Add solution for 0x05c-3

---
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _strcmp - Compare two strings
+ * @s1: string
+ * @s2: string
+ * Return: negative int if s1 < s2, 0 if matching, and positive int if s1 > s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] > s2[i])
+			return (15);
+		else if (s1[i] < s2[i])
+			return (-15);
+	}
+	return (0);
+}

dd30734e2f9b9c2b4f8344bf4aecd7b4a311cc86
philipyoo <philipyoo10@gmail.com>
2016-10-27 22:37:40 +0000

    Add solution for 0x05c-5

---
+++ 0x05-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * string_toupper - Change all lowercase letters to uppercase
+ * @c: string
+ * Return: c
+ */
+
+char *string_toupper(char *c)
+{
+	int i;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		if (c[i] > 96 && c[i] < 123)
+		{
+			c[i] -= 32;
+		}
+	}
+	return c;
+}

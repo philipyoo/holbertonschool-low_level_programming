d0964a571bbad3f4454be2f7d4c5e6d8628f20b9
philipyoo <philipyoo10@gmail.com>
2016-10-28 01:11:05 +0000

    Add 0x05c-7 leeet code

---
+++ 0x05-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * leet - encode a string into 1337
+ * @s: string
+ * Return: encoded string `s`
+ */
+
+char *leet(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		while (s[i] == 'a' || s[i] == 'A')
+			s[i] = '4';
+		while (s[i] == 'e' || s[i] == 'E')
+			s[i] = '3';
+		while (s[i] == 'o' || s[i] == 'O')
+			s[i] = '0';
+		while (s[i] == 't' || s[i] == 'T')
+			s[i] = '7';
+		while (s[i] == 'l' || s[i] == 'L')
+			s[i] = '1';
+	}
+
+	return (s);
+}

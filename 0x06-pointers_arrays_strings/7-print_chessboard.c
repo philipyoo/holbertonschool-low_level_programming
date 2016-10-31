d13ab9fa42b31e8a98b183d9519993e4308da77a
Philip <philipyoo10@gmail.com>
2016-10-31 23:36:44 +0000

    Add solution for 0x06c-6

---
+++ 0x06-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_chessboard - print the chessboard, 2d array
+ * @a: 2d array of chars
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int y, x;
+
+	y = 0;
+	while (y < 8)
+	{
+		x = 0;
+		while (x < 8)
+		{
+			_putchar(a[y][x]);
+			x++;
+		}
+		_putchar('\n');
+		y++;
+	}
+}
--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -5,3 +5,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);

5a03c261daae7f2c6825ac5f4b2a8c89e271734c
philipyoo <philipyoo10@gmail.com>
2016-11-01 19:48:06 +0000

    Edit solution for 0x06c-5

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -17,15 +17,18 @@ char *_strstr(char *haystack, char *needle)
 	while (haystack[i] != '\0')
 	{
 		j = 0;
-		while (needle[j + c] != '\0' && haystack[i + c] != '\0'
-		       && needle[j + c] == haystack[i + c])
+		if (needle[j + c] == haystack[i + c])
 		{
-			if (haystack[i + c] != needle[j + c])
-				break;
-			c++;
+			while (needle[j + c] != '\0' && haystack[i + c] != '\0'
+			       && needle[j + c] == haystack[i + c])
+			{
+				if (haystack[i + c] != needle[j + c])
+					break;
+				c++;
+			}
+			if (needle[j + c] == '\0')
+				return (&haystack[i]);
 		}
-		if (needle[j + c] == '\0')
-			return (&haystack[i]);
 		j++;
 		i++;
 	}

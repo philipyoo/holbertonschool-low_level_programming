d7025e4a8f788891d3ef1eeee182c82fe6a99aca
Philip <philipyoo10@gmail.com>
2016-11-01 00:35:01 +0000

    Add solution for 0x06c-8

---
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_diagsums - Print the sum of the 2 diagonals of a square matrix of ints
+ * @a: 2d array of int types
+ * @size: size of array (square)
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, sum, sizer;
+
+	i = 0;
+	sum = 0;
+	sizer = size * size;
+	/* sum of 1 diag, sum of 2 diag */
+	/* first diag */
+	while (i < sizer)
+	{
+		if (i % (size + 1) == 0)
+			sum += a[i];
+		i++;
+	}
+	printf("%d, ", sum);
+
+	sum = 0;
+	i = 0;
+	while (i < sizer)
+	{
+		if (i % (size - 1) == 0 && i != (sizer - 1))
+			sum += a[i];
+		i++;
+	}
+	printf("%d\n", sum);
+}

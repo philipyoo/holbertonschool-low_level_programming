1751850083938087be1328a62792fa3f12fe0716
philipyoo <philipyoo10@gmail.com>
2016-11-02 00:24:54 +0000

    Edit solution for 0x04c-11

--- 0x04-pointers_arrays_strings/101-keygen.c
+++ 0x04-pointers_arrays_strings/101-keygen.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main(void)
 {
@@ -9,12 +10,12 @@ int main(void)
 
 	for (count = 0, total = 2772; total > 122; count++)
 	{
+		srand(time(NULL));
 		myrand = (rand() % 122) + 1;
 		printf("%c", myrand);
 		total -= myrand;
 	}
 	printf("%c", total);
-
 
 	return (0);
 }

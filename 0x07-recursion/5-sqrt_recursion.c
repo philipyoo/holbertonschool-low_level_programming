73549a674d6ef08a3e46bb5d4375b5ff51183f6e
philipyoo <philipyoo10@gmail.com>
2016-11-02 20:29:21 +0000

    Edit solution for 0x07c-5

--- 0x07-recursion/5-sqrt_recursion.c
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -1,17 +1,23 @@
 #include "holberton.h"
 
-int halp(int c, int i);
-
 /**
  * _sqrt_recursion - return the natural square root of a number
  * @n: int number
- * Return: If no natural square root, return -1. Else return natural square root
+ * Return: If no natural square root, return -1. Else return natural
+ * square root
  */
 
 int _sqrt_recursion(int n)
 {
 	return (halp(n, 1));
 }
+
+/**
+ * halp - helper function to solve _sqrt_recursion
+ * @c: number to determine if square root
+ * @i: incrementer to compare against `c`
+ * Return: square root if natural square root, or -1 if none found
+ */
 
 int halp(int c, int i)
 {
--- 0x07-recursion/holberton.h
+++ 0x07-recursion/holberton.h
@@ -5,4 +5,5 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int halp(int c, int i);
 int is_prime_number(int n);

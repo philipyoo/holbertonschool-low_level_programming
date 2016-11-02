7fb1b808aa6a2c022aa99f78db918d04fa7c7a75
philipyoo <philipyoo10@gmail.com>
2016-11-02 00:30:54 +0000

    Edit solution for 0x04c-11, maybe works?

--- 0x04-pointers_arrays_strings/101-keygen.c
+++ 0x04-pointers_arrays_strings/101-keygen.c
@@ -2,18 +2,16 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define MAX 125
-#define MIN 1
-
 int main(void)
 {
 	int myrand;
 	int count;
 	int total;
 
+	srand(time(NULL));
 	for (count = 0, total = 2772; total > 122; count++)
 	{
-		myrand = (rand() % (MAX + 1 - MIN)) + MIN;
+		myrand = (rand() % 125) + 1;
 		printf("%c", myrand);
 		total -= myrand;
 	}

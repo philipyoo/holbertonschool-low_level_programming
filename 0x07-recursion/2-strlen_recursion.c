ec691adccbdfb6f8ca65c3900cb47c9e49eba78a
philipyoo <philipyoo10@gmail.com>
2016-11-02 17:52:20 +0000

    Edit solutions for 0x07c 2 and 3

--- 0x07-recursion/2-strlen_recursion.c
+++ 0x07-recursion/2-strlen_recursion.c
@@ -3,6 +3,7 @@
 /**
  * _strlen_recursion - return length of a string
  * @s: string
+ * Return: length of string
  */
 
 int _strlen_recursion(char *s)
@@ -11,11 +12,11 @@ int _strlen_recursion(char *s)
 
 	c = 0;
 	if (*s == '\0')
-		return 0;
+		return (0);
 	else
 	{
 		c = 1 +  _strlen_recursion(s + 1);
 	}
 
-	return c;
+	return (c);
 }
--- 0x07-recursion/3-factorial.c
+++ 0x07-recursion/3-factorial.c
@@ -3,14 +3,15 @@
 /**
  * factorial - return factorial of a given number.
  * @n: int type number
+ * Return: factorial of number
  */
 
 int factorial(int n)
 {
 	if (n < 0)
-		return -1;
+		return (-1);
 	else if (n < 2)
-		return n;
+		return (n);
 	else
-		return n * factorial(n - 1);
+		return (n * factorial(n - 1));
 }

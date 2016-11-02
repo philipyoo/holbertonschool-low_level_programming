5a355cbc6b927d0da81990c1f31b7a8ec358f5ac
philipyoo <philipyoo10@gmail.com>
2016-11-02 16:42:47 +0000

    Add solution for 0x07c-1

---
+++ 0x07-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - print a string in reverse
+ * @s: string
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+	else
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}

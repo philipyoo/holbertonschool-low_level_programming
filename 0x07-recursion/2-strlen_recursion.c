434d1dc9a6d0b39d7ed6fb67d948cfa0b5663189
philipyoo <philipyoo10@gmail.com>
2016-11-02 19:31:37 +0000

    Edit solution for 0x07c-2

--- 0x07-recursion/2-strlen_recursion.c
+++ 0x07-recursion/2-strlen_recursion.c
@@ -13,10 +13,8 @@ int _strlen_recursion(char *s)
 	c = 0;
 	if (*s == '\0')
 		return (0);
-	else
-	{
-		c = 1 +  _strlen_recursion(s + 1);
-	}
+
+	c = 1 +  _strlen_recursion(s + 1);
 
 	return (c);
 }

ab1bd58d0da7443a6b4a699d615a0c55c59f53fd
philipyoo <philipyoo10@gmail.com>
2016-11-06 19:43:45 +0000

    Add solution for 0x09c-0

---
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - print the name of the file followed by a new line
+ * Return: 0
+ */
+
+int main (int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}

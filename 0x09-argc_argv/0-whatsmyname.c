6b39d3e05fe20a8f5e200f17b0f8cdb46e7b901a
philipyoo <philipyoo10@gmail.com>
2016-11-06 19:47:25 +0000

    Edit solution for 0x09c-0

--- 0x09-argc_argv/0-whatsmyname.c
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -3,10 +3,12 @@
 
 /**
  * main - print the name of the file followed by a new line
+ * @argc: argument count
+ * @argv: arguments passed to main as an array of strings
  * Return: 0
  */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	(void) argc;
 

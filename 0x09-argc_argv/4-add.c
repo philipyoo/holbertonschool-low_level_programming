63306eb523d2cce81e0af4e45d7b47886cf08a26
philipyoo <philipyoo10@gmail.com>
2016-11-06 20:54:39 +0000

    Edit solution for 0x09c-4

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -4,10 +4,10 @@
 
 /**
  * main - add 2 positive numbers and print the result
- * @Description: If no number is passed to the program, print 0.
- * If one of the numbers contain non-digits, print Error.
  * @argc: argument count
  * @argv: argument vector, array of strings
+ * Description: If no number is passed to program, print 0.
+ * If one of the numbers contain non-digits, print Error.
  * Return: 1 if error, 0 if function runs properly.
  */
 

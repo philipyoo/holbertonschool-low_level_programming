9827dad6c8bf54380b44d6c2493165cc7417fa83
philipyoo <philipyoo10@gmail.com>
2016-11-06 20:26:59 +0000

    Edit solution for 0x09c-3, change returns based on condition

--- 0x09-argc_argv/3-mul.c
+++ 0x09-argc_argv/3-mul.c
@@ -6,7 +6,7 @@
  * main - multiply 2 numbers passed to main, or Error
  * @argc: argument count
  * @argv: argument vector
- * Return: 1
+ * Return: 1 if error, 0 if function runs correctly
  */
 
 int main(int argc, char *argv[])
@@ -16,6 +16,7 @@ int main(int argc, char *argv[])
 	if (argv[1] && argv[2])
 	{
 		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		return (0);
 	}
 	else
 		printf("Error\n");

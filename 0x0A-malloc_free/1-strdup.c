87c23148c95cff805715f01f73ecbabc884ccbb2
philipyoo <philipyoo10@gmail.com>
2016-11-08 21:56:51 +0000

    Add solution for 0x0A-1

---
+++ 0x0A-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - Duplicate a string using malloc
+ * @str: string to duplicate
+ * Return: Pointer to a the new duped string
+ */
+
+char *_strdup(char *str)
+{
+	char *a;
+	int i, c;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	a = malloc(i * sizeof(*a));
+
+	for (c = 0; c < i; c++)
+		a[c] = str[c];
+	a[c] = '\0';
+
+	return (a);
+}

8fbb5bf60c8da0173ebefce2a66f35594dbc7548
philipyoo <philipyoo10@gmail.com>
2016-11-08 23:25:06 +0000

    Add solution for 0x0A-4

---
+++ 0x0A-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - free up a 2d grid
+ * @grid: double pointer 2d grid
+ * @height: height of grid
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}

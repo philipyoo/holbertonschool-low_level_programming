655f3ff887af1475aa254911d08348831e5a3d5d
philipyoo <philipyoo10@gmail.com>
2016-11-08 22:03:06 +0000

    Add solution for 0x0A-2

---
+++ 0x0A-malloc_free/2-str_concat.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - concatenate two strings using malloc
+ * @s1: string 1
+ * @s2: string 2
+ * Return: pointer to concat string
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *a;
+	int i, j, c, d;
+
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+
+	a = malloc((i * sizeof(*s1)) + (j * sizeof(*s2)));
+
+	for (c = 0, d = 0; c < (i + j); c++)
+	{
+		if (c < i)
+			a[c] = s1[c];
+		else
+			a[c] = s2[d++];
+	}
+
+	return (a);
+}

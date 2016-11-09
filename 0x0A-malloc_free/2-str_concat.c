be4f492007d07fc0048b8388d78b1947ec7f26bd
philipyoo <philipyoo10@gmail.com>
2016-11-09 18:32:37 +0000

    Edit solution for 0x0A-2, checking if right

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 #include <stdlib.h>
-
+#include <stdio.h>
 /**
  * str_concat - concatenate two strings using malloc
  * @s1: string 1
@@ -13,7 +13,7 @@ char *str_concat(char *s1, char *s2)
 	char *a;
 	int i, j, c, d;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL && s2 == NULL)
 		return (NULL);
 
 	for (i = 0; s1[i] != '\0'; i++)
@@ -25,13 +25,21 @@ char *str_concat(char *s1, char *s2)
 	if (a == NULL)
 		return (NULL);
 
-	for (c = 0, d = 0; c < (i + j + 1); c++)
+	for (c = 0, d = 0; c < (i + j + 1) && s2[d] != '\0'; c++)
 	{
 		if (c < i)
 			a[c] = s1[c];
 		else
 			a[c] = s2[d++];
 	}
+	a[c] = '\0';
+
+/*	for (i = 0; a[i] != '\0'; i++)
+	{
+		printf("%d: %c\n", i, a[i]);
+	}
+	printf("%d, %lu ", i, ((i * sizeof(*s1)) + (j * sizeof(*s2)) + 1));
+*/
 
 	return (a);
 }

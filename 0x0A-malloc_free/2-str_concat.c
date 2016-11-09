29dcb1754333879559e75e2f3929d6f23b1520e2
philipyoo <philipyoo10@gmail.com>
2016-11-09 18:07:21 +0000

    Edit solution for 0x0A-2

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -13,12 +13,15 @@ char *str_concat(char *s1, char *s2)
 	char *a;
 	int i, j, c, d;
 
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+
 	for (i = 0; s1[i] != '\0'; i++)
 		;
 	for (j = 0; s2[j] != '\0'; j++)
 		;
 
-	a = malloc((i * sizeof(*s1)) + (j * sizeof(*s2)));
+	a = malloc((i * sizeof(*s1)) + (j * sizeof(*s2)) + 2);
 	if (a == NULL)
 		return (NULL);
 

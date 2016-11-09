8fe3d7458c643633319c38b1892a17b03abd8dec
philipyoo <philipyoo10@gmail.com>
2016-11-09 18:03:20 +0000

    Edit solution for 0x0A-1

--- 0x0A-malloc_free/1-strdup.c
+++ 0x0A-malloc_free/1-strdup.c
@@ -12,15 +12,16 @@ char *_strdup(char *str)
 	char *a;
 	int i, c;
 
+	if (str == NULL)
+		return (NULL);
+
 	for (i = 0; str[i] != '\0'; i++)
 		;
 
-	a = malloc(i * sizeof(*a));
+	a = malloc(i * sizeof(*a) + 1);
 	if (a == NULL)
-	{
-		free(a);
 		return (NULL);
-	}
+
 	for (c = 0; c < i; c++)
 		a[c] = str[c];
 	a[c] = '\0';

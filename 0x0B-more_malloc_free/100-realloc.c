f42afb14896382aadec2fcf8c8430dc6dcf8c700
philipyoo <philipyoo10@gmail.com>
2016-11-10 23:38:25 +0000

    Add solution for 0x0B-4

---
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+
+/**
+ * _realloc - reallocates a memory block
+ * @ptr: pointer to previous memory block
+ * @old_size: size in bytes of allocated space for `ptr`
+ * @new_size: size in bytes for new allocated space
+ * Return: Pointer to new memory block, or NULL if error
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *p, *copy;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr != NULL && new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (ptr == NULL)
+	{
+		p = malloc(new_size);
+		if (p == NULL)
+			return (NULL);
+		return (p);
+	}
+
+	p = malloc(new_size);
+	if (p == NULL)
+		return (NULL);
+	copy = ptr;
+	for (i = 0; i < old_size; i++)
+		p[i] = copy[i];
+	free(ptr);
+	return (p);
+}
--- 0x0B-more_malloc_free/holberton.h
+++ 0x0B-more_malloc_free/holberton.h
@@ -5,3 +5,4 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 int _strlen(char *s);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);

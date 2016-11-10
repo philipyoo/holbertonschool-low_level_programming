e1a4cbe5c7dbddee579f2ff81311b5570a156962
philipyoo <philipyoo10@gmail.com>
2016-11-10 22:31:28 +0000

    Add solution for 0x0B-2

---
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * _calloc - allocates memory for an array given number of elements and size
+ * @nmemb: number of elements
+ * @size: size of each element
+ * Return: Null if error, else pointer to allocated memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *p;
+	unsigned int i;
+
+	if (nmemb <= 0 || size <= 0)
+		return (NULL);
+
+	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+		p[i] = 0;
+
+	return (p);
+}
--- 0x0B-more_malloc_free/holberton.h
+++ 0x0B-more_malloc_free/holberton.h
@@ -3,3 +3,4 @@ int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 int _strlen(char *s);
+void *_calloc(unsigned int nmemb, unsigned int size);

2320ac07047567d3f5cf5fef5c0a73f9ef8a254c
philipyoo <philipyoo10@gmail.com>
2016-11-10 22:39:32 +0000

    Add solution for 0x0B-3

--- 0x0B-more_malloc_free/holberton.h
+++ 0x0B-more_malloc_free/holberton.h
@@ -4,3 +4,4 @@ void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 int _strlen(char *s);
 void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);

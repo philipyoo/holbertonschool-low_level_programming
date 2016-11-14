0e6c42cb1b9b0fb0f887cd5263bc64879e37e777
philipyoo <philipyoo10@gmail.com>
2016-11-14 23:57:47 +0000

    Add solution for 0x0D-3

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -7,12 +7,12 @@
  * @age: integer
  * @owner: character string
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);

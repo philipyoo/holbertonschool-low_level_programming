5fe43526c32fd24d43fb43a5f50c9cf9a68e06b0
philipyoo <philipyoo10@gmail.com>
2016-11-14 23:51:41 +0000

    Edit solution for 0x0D-2

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -13,8 +13,7 @@ void print_dog(struct dog *d)
 	{
 		printf("Name: ");
 		d->name == NULL ? printf("(nil)\n") : printf("%s\n", d->name);
-		printf("Age: ");
-		(int)d->age <= 0 ? printf("(nil)\n") : printf("%f\n", d->age);
+		printf("Age: %f\n", d->age);
 		printf("Owner: ");
 		d->owner == NULL ? printf("(nil)\n") : printf("%s\n", d->owner);
 	}

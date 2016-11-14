e6c15d3084e5c24507fb7bdd93060295e05d8ea0
philipyoo <philipyoo10@gmail.com>
2016-11-14 22:44:50 +0000

    Edit solution for 0x0D-0

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - structure definition of a dog
+ * @name: character string
+ * @age: integer
+ * @owner: character string
+ */
 struct dog
 {
 	char *name;

b450c5f381e7b38c062f66d32972d929041fd69e
philipyoo <philipyoo10@gmail.com>
2016-11-15 18:11:40 +0000

    Edit solution for 0x0D-4, 5

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -11,14 +11,32 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	struct dog *doge;
+	int i, j, k;
+	char *n, *o;
 
 	doge = malloc(sizeof(struct dog));
 	if (doge == NULL)
 		return (NULL);
 
-	doge->name = name;
+	for (i = 0; name[i] != '\0'; i++)
+		;
+	for (j = 0; owner[j] != '\0'; j++)
+		;
+	n = malloc(sizeof(char) * i + 1);
+	if (n == NULL)
+		return (NULL);
+	o = malloc(sizeof(char) * j + 1);
+	if (o == NULL)
+		return (NULL);
+
+	for (k = 0; k < i; k++)
+		*(n + k) = name[k];
+	for (k = 0; k < j; k++)
+		*(o + k) = owner[k];
+
+	doge->name = n;
 	doge->age = age;
-	doge->owner = owner;
+	doge->owner = o;
 
 	return (doge);
 }
--- 0x0D-structures_typedef/5-free_dog.c
+++ 0x0D-structures_typedef/5-free_dog.c
@@ -8,9 +8,11 @@
 
 void free_dog(dog_t *d)
 {
-	free(d->name);
-	free(d->age);
-	free(d->owner);
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
 
-	free(d);
+		free(d);
+	}
 }

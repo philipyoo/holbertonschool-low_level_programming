6ab2c848e5dabd7daf8845c6c680a80489409c0b
philipyoo <philipyoo10@gmail.com>
2016-11-17 05:19:28 +0000

    Edit solution for 0x0E-3

--- 0x0E-function_pointers/3-get_op_func.c
+++ 0x0E-function_pointers/3-get_op_func.c
@@ -5,7 +5,6 @@
 /**
  * get_op_func - select the correct operation function asked by user
  * @s: operator argument
- * @
  * Return: function pointer corresponding to operator given
  */
 int (*get_op_func(char *s))(int, int)

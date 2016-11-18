db640035fd6778902c39d098f8783014cfa12841
philipyoo <philipyoo10@gmail.com>
2016-11-18 23:14:42 +0000

    Edit solution for 0x0F-1

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -11,13 +11,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list list;
-
-	if (separator == NULL)
-		return;
+	char *hold;
 
 	va_start(list, n);
 	for (i = 0; i < n; i++)
 	{
+		hold = va_arg(list, int);
+		if (hold == NULL)
+			continue;
 		printf("%d", va_arg(list, int));
 		if (i != (n - 1))
 			printf("%s", separator);

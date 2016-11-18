20dfaf62043b1515f99a71744cdd268c115f9436
philipyoo <philipyoo10@gmail.com>
2016-11-18 19:01:54 +0000

    Add solution for 0x0F-1

---
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - print each number with separator, followed by a newline
+ * @separator: string to be printed between numbers
+ * @n: number of args passed
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list list;
+
+	if (separator == NULL)
+		return;
+
+	va_start(list, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(list, int));
+		if (i != (n - 1))
+			printf("%s", separator);
+	}
+	printf("\n");
+}

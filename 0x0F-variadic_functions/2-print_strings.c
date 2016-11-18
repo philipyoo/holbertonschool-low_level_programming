df84be045f5c85b2f564abc165cf32421be7d14e
philipyoo <philipyoo10@gmail.com>
2016-11-18 23:18:43 +0000

    Edit solution for 0x0F-2

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("(nil)");
 		else
 			printf("%s", hold);
-		if (i != (n - 1))
+		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
 	printf("\n");

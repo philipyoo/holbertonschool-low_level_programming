7f71efd1c5f19f3c2ce8b92a36d7cdea743bb18a
philipyoo <philipyoo10@gmail.com>
2016-11-20 00:05:43 +0000

    Edit solution for 0x0F-3

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -2,31 +2,31 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-/**
- * _findlast - find the last valid type in `format`
- * @format: string of formats passed from main function
- * Result: return index of last valid type
- */
-int _findlast(const char * const format)
+void _printchar(va_list list)
 {
-	unsigned int i, last;
-
-	i = 0, last = 0;
-	while (format[i] != '\0')
-	{
-		switch (format[i])
-		{
-		case 'c':
-		case 'i':
-		case 'f':
-		case 's':
-			last = i;
-			break;
-		}
-		i++;
-	}
-	return (last);
+	printf("%c", va_arg(list, int));
 }
+
+void _printstr(va_list list)
+{
+	char *s;
+
+	s = va_arg(list, char *);
+	if (s == NULL)
+		s = "(nil)";
+	printf("%s", s);
+}
+
+void _printfloat(va_list list)
+{
+	printf("%f", va_arg(list, double));
+}
+
+void _printint(va_list list)
+{
+	printf("%d", va_arg(list, int));
+}
+
 
 /**
  * print_all - print anything passed if char, int, float, or string.
@@ -34,39 +34,28 @@ int _findlast(const char * const format)
  */
 void print_all(const char * const format, ...)
 {
-	unsigned int i, on, last;
+	unsigned int i, j;
 	va_list args;
-	char *str;
+	char *sep;
 
-	last = _findlast(format);
-	i = 0, on = 0;
+	checker storage[] = {
+		{ "c", _printchar },
+		{ "f", _printfloat },
+		{ "s", _printstr },
+		{ "i", _printint }
+	};
+
+	i = 0;
+	sep = "";
 	va_start(args, format);
-	while (format[i] != '\0' || i < last)
+	while (format[i / 4] != '\0')
 	{
-		if (on && i <= last)
-			printf(", ");
-		on = 0;
-		switch (format[i])
+		j = i % 4;
+		if (storage[j].type[0] == format[i / 4])
 		{
-		case 'c':
-			printf("%c", va_arg(args, int));
-			on = 1;
-			break;
-		case 'i':
-			printf("%d", va_arg(args, int));
-			on = 1;
-			break;
-		case 'f':
-			printf("%f", va_arg(args, double));
-			on = 1;
-			break;
-		case 's':
-			str = va_arg(args, char *);
-			if (str == NULL)
-				str = "(nil)";
-			printf("%s", str);
-			on = 1;
-			break;
+			printf("%s", sep);
+			storage[j].f(args);
+			sep = ", ";
 		}
 		i++;
 	}
--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -5,4 +5,14 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void _printchar(va_list list);
+void _printstr(va_list list);
+void _printfloat(va_list list);
+void _printint(va_list list);
+
+typedef struct checker
+{
+	char *type;
+	void (*f)();
+} checker;
 #endif

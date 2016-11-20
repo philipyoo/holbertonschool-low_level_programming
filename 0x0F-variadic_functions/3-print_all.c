2772c9faecb52e91f36bea846816611f0b9fb6e1
philipyoo <philipyoo10@gmail.com>
2016-11-20 00:20:23 +0000

    Edit solution for 0x0F-3

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -2,11 +2,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * _printchar - print char type element from va_list
+ * @list: va_list passed to function
+ */
 void _printchar(va_list list)
 {
 	printf("%c", va_arg(list, int));
 }
 
+/**
+ * _printstr - print string element from va_list
+ * @list: va_list passed to function
+ */
 void _printstr(va_list list)
 {
 	char *s;
@@ -17,16 +25,23 @@ void _printstr(va_list list)
 	printf("%s", s);
 }
 
+/**
+ * _printfloat - print float type element from va_list
+ * @list: va_list passed to function
+ */
 void _printfloat(va_list list)
 {
 	printf("%f", va_arg(list, double));
 }
 
+/**
+ * _printint - print int type element from va_list
+ * @list: va_list passed to function
+ */
 void _printint(va_list list)
 {
 	printf("%d", va_arg(list, int));
 }
-
 
 /**
  * print_all - print anything passed if char, int, float, or string.
@@ -48,7 +63,7 @@ void print_all(const char * const format, ...)
 	i = 0;
 	sep = "";
 	va_start(args, format);
-	while (format[i / 4] != '\0')
+	while (format[i / 4] != '\0' && format != NULL)
 	{
 		j = i % 4;
 		if (storage[j].type[0] == format[i / 4])

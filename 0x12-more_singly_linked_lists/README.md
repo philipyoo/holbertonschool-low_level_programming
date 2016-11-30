a3c655b6125d4823f18ed110c5de998d5df30afd
philipyoo <philipyoo10@gmail.com>
2016-11-30 19:28:56 +0000

    Create 0x12 dir, header file, and readme

---
+++ 0x12-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+# 0x12. C - More singly linked lists
---
+++ 0x12-more_singly_linked_lists/lists.h
@@ -0,0 +1,17 @@
+#ifndef LISTS_H
+#define LISTS_H
+#include <stdlib.h>
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+#endif

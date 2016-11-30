211c12146ca824d305d5629f2f37cc4675e3e9d0
philipyoo <philipyoo10@gmail.com>
2016-11-30 21:41:01 +0000

    Add solution for 0x12-6

---
+++ 0x12-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * pop_listint - Delete the head node of a `listint_t` linked list
+ * @head: double pointer to head node of linked list
+ * Return: head nodes data `n`
+ */
+int pop_listint(listint_t **head)
+{
+	int num;
+	listint_t *hold;
+
+	hold = *head;
+	num = hold->n;
+	*head = hold->next;
+	free(hold);
+
+	return (num);
+}
--- 0x12-more_singly_linked_lists/lists.h
+++ 0x12-more_singly_linked_lists/lists.h
@@ -21,4 +21,5 @@ listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 void free_listint2(listint_t **head);
+int pop_listint(listint_t **head);
 #endif

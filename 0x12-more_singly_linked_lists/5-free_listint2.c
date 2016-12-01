2151ab24a92e45f6f3f890e37a0816e7381ee593
philipyoo <philipyoo10@gmail.com>
2016-12-01 19:00:30 +0000

    Edit solution for 0x12-5

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -8,12 +8,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *hold;
 
+	if (head == NULL)
+		return;
+
 	while (*head != NULL)
 	{
 		hold = *head;
 		*head = (*head)->next;
 		free(hold);
 	}
-
-	*head = NULL;
 }

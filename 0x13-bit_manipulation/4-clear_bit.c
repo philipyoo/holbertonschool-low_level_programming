beb4d02d7ad3b24acd9c758400007ee68b5df524
philipyoo <philipyoo10@gmail.com>
2016-12-05 23:17:06 +0000

    Add solution for 0x13-4

---
+++ 0x13-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * clear_bit - set the value of a bit to 0 at a given index
+ * @n: pointer to decimal number to change
+ * @index: index position to change
+ * Return: 1 if it worked, -1 if error
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int i;
+
+	if (index > 64)
+		return (-1);
+
+	for (i = 1; index > 0; i *= 2, index--)
+		;
+
+	if (i <= *n)
+		*n -= i;
+
+	return (1);
+}

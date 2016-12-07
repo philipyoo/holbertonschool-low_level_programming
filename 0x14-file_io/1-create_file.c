75779bd207c6017468f592fec241f93e692f705b
philipyoo <philipyoo10@gmail.com>
2016-12-07 22:45:08 +0000

    Add solution for 0x14-1

---
+++ 0x14-file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * create_file - create a file with read/write access for user
+ * @filename: name of file to create
+ * @text_content: string to write to file
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, rstatus, i;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	if (fd == -1)
+		return (-1);
+
+	for (i = 0; text_content[i] != '\0'; i++)
+		;
+
+	rstatus = write(fd, text_content, i);
+	if (rstatus == -1)
+		return (-1);
+
+	close(fd);
+	return (1);
+}

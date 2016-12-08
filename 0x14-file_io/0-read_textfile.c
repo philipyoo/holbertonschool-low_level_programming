7460d7b51c926201d2e4b1792c41d329e47229da
philipyoo <philipyoo10@gmail.com>
2016-12-08 02:18:29 +0000

    Edit solution for 0x14-0

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -9,13 +9,13 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t rcount;
+	ssize_t rcount, wcount;
 	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
+	fd = open(filename, O_RDWR);
 	if (fd == -1)
 		return (0);
 
@@ -28,7 +28,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	rcount = read(fd, buffer, letters);
 	if (rcount == -1)
 		return (0);
-	printf("%s", buffer);
+
+	wcount = write(1, buffer, rcount);
+	if (wcount == -1)
+		return (0);
 	free(buffer);
 
 	close(fd);

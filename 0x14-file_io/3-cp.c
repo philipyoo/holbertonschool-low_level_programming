239c43531a0c798d7b847064ee6b135ec3566217
philipyoo <philipyoo10@gmail.com>
2016-12-08 18:42:51 +0000

    Edit solution for 0x14-3

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -1,6 +1,7 @@
 #include "holberton.h"
 
 #define MAXSIZE 1204
+#define SE STDERR_FILENO
 
 /**
  * main - create the copy bash script
@@ -16,10 +17,10 @@ int main(int ac, char *av[])
 
 	mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 	if (ac != 3)
-		dprintf(2, "Usage: cp file_from file_to\n"), exit(97);
+		dprintf(SE, "Usage: cp file_from file_to\n"), exit(97);
 	input_fd = open(av[1], O_RDONLY);
 	if (input_fd == -1)
-		dprintf(2, "Error: Can't read from file %s\n", av[1]), exit(98);
+		dprintf(SE, "Error: Can't read from file %s\n", av[1]), exit(98);
 	output_fd = open(av[2], O_CREAT | O_RDWR | O_TRUNC, mode);
 	if (output_fd == -1)
 		dprintf(2, "Error: Can't write to %s\n", av[2]), exit(99);
@@ -27,21 +28,21 @@ int main(int ac, char *av[])
 		istatus = read(input_fd, buf, MAXSIZE);
 		if (istatus == -1)
 		{
-			dprintf(2, "Error: Can't read from file %s\n", av[1]);
+			dprintf(SE, "Error: Can't read from file %s\n", av[1]);
 			exit(98);
 		}
 		ostatus = write(output_fd, buf, (ssize_t) istatus);
 		if (ostatus == -1)
 		{
-			dprintf(2, "Error: Can't write to %s\n", av[2]);
+			dprintf(SE, "Error: Can't write to %s\n", av[2]);
 			exit(99);
 		}
 	} while (istatus > 0);
 	istatus = close(input_fd);
 	if (istatus == -1)
-		dprintf(2, "Error: Can't close fd %d\n", input_fd), exit(100);
+		dprintf(SE, "Error: Can't close fd %d\n", input_fd), exit(100);
 	ostatus = close(output_fd);
 	if (ostatus == -1)
-		dprintf(2, "Error: Can't close fd %d\n", output_fd), exit(100);
+		dprintf(SE, "Error: Can't close fd %d\n", output_fd), exit(100);
 	return (0);
 }

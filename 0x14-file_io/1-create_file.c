492638b660e70c708312dfe45e379a44cacc1821
philipyoo <philipyoo10@gmail.com>
2016-12-08 07:12:02 +0000

    Edit solution for 0x14-1

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -13,7 +13,8 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	printf("testing: %d\n", fd);
 	if (fd == -1)
 		return (-1);
 

5454bc2a709545961c81c74dcf8e5a0102d6b3ee
philipyoo <philipyoo10@gmail.com>
2016-12-08 06:48:53 +0000

    Edit solution for 0x14-1, use symbolic constants

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 

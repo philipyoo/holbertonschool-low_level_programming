b12b116718c223276dd0da874c25988d7f1a08ad
philipyoo <philipyoo10@gmail.com>
2016-12-08 03:18:40 +0000

    Edit solution for 0x14-1

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -17,12 +17,14 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	for (i = 0; text_content[i] != '\0'; i++)
-		;
-	if (i != 0)
+	if (text_content)
+	{
+		for (i = 0; text_content[i] != '\0'; i++)
+			;
 		rstatus = write(fd, text_content, i);
-	if (rstatus == -1)
-		return (-1);
+		if (rstatus == -1)
+			return (-1);
+	}
 
 	close(fd);
 	return (1);

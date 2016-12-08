ba55332a24f16cacfe686fb85372634d6895117e
philipyoo <philipyoo10@gmail.com>
2016-12-08 09:06:07 +0000

    Edit solution for 0x14-3

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -14,14 +14,22 @@ int main(int ac, char *av[])
 	char buf[MAXSIZE];
 
 	if (ac != 3)
-		dprintf(2, "Usage: cp file_from file_to\n"); exit(97);
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
 	input_fd = open(av[1], O_RDONLY);
 	if (input_fd == -1)
-		dprintf(2, "Error: Can't read from file %s\n", av[1]); exit(98);
+	{
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
 	output_fd = open(av[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
 	if (output_fd == -1)
-		dprintf(2, "Error: Can't write to %s\n", av[2]); exit(99);
-
+	{
+		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
 	do {
 		istatus = read(input_fd, buf, MAXSIZE);
 		if (istatus == -1)
@@ -36,12 +44,17 @@ int main(int ac, char *av[])
 			exit(99);
 		}
 	} while (istatus > 0);
-
 	istatus = close(input_fd);
 	if (istatus == -1)
-		dprintf(2, "Error: Can't close fd %d\n", input_fd); exit(100);
+	{
+		dprintf(2, "Error: Can't close fd %d\n", input_fd);
+		exit(100);
+	}
 	ostatus = close(output_fd);
 	if (ostatus == -1)
-		dprintf(2, "Error: Can't close fd %d\n", output_fd); exit(100);
+	{
+		dprintf(2, "Error: Can't close fd %d\n", output_fd);
+		exit(100);
+	}
 	return (0);
 }

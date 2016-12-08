80abfa50ca457700b015472963efd42756c61a34
philipyoo <philipyoo10@gmail.com>
2016-12-08 07:14:01 +0000

    Edit 0x14-1, remove printf tests

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -14,7 +14,6 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-	printf("testing: %d\n", fd);
 	if (fd == -1)
 		return (-1);
 

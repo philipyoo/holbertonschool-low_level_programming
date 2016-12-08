71ef58472d886c90e05dbc3ca4288ff2f567fc32
philipyoo <philipyoo10@gmail.com>
2016-12-08 04:00:23 +0000

    Add solution for 0x14-3

---
+++ 0x14-file_io/3-cp.c
@@ -0,0 +1,51 @@
+#include "holberton.h"
+
+#define MAXSIZE 1204
+
+/**
+ * main - create the copy bash script
+ * @ac: argument count
+ * @av: arguments as strings
+ * Return: 0
+ */
+int main(int ac, char *av[])
+{
+	int input_fd, output_fd, istatus, ostatus;
+	char buf[MAXSIZE];
+
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	input_fd = open(av[1], O_RDONLY);
+	if (input_fd == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	output_fd = open(av[2], O_CREAT | O_RDWR, 0664);
+	if (output_fd == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+
+	do
+	{
+		istatus = read(input_fd, buf, MAXSIZE);
+		if (istatus == -1)
+		{
+			dprintf(2, "Error: Can't read from file %s\n", av[1]);
+			exit(98);
+		}
+		ostatus = write(output_fd, buf, (ssize_t) istatus);
+		if (ostatus == -1)
+		{
+			dprintf(2, "Error: Can't write to %s\n", av[2]);
+			exit(99);
+		}
+	} while (istatus > 0);
+	return (0);
+}

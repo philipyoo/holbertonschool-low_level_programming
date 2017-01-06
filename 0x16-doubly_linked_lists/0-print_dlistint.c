27e5d2e9b406240aa2432c4b13cbabd2a4c1b2ac
philipyoo <philipyoo10@gmail.com>
2017-01-06 23:04:46 +0000

    Create 0x16c dir, add solution for 0x16-0

---
+++ 0x16-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - Print all the values of each node in a `dlistint_t` list
+ * @h: head pointer in doubly linked list
+ * Return: Number of nodes in LL
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *temp;
+	size_t count;
+
+	temp = h;
+	while (temp->prev)
+		temp = temp->prev;
+
+	for (count = 0; temp; count++, temp = temp->next)
+		printf("%d\n", temp->n);
+
+	return (count);
+}

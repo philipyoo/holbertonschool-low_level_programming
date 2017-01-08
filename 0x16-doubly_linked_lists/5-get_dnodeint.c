bfe7d641c5eb6a80e7ae890931edca40221dd451
philipyoo <philipyoo10@gmail.com>
2017-01-08 23:50:26 +0000

    Add solution for 0x16-5

---
+++ 0x16-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - get the nth node of a `dlistint_t` doubly linked list
+ * @head: pointer to head of LL
+ * @index: node index to return
+ * Return: node at index given or NULL if node does not exist
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *tmp;
+	unsigned int c;
+
+	tmp = head;
+	for (c = 0; tmp && c < index; tmp = tmp->next, c++)
+		;
+
+	return (tmp);
+}

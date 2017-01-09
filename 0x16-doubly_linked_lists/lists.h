eb8706481ec2f84ffbda907a0336ea1322463ed5
philipyoo <philipyoo10@gmail.com>
2017-01-09 21:25:40 +0000

    Add edited lists header file

--- 0x16-doubly_linked_lists/lists.h
+++ 0x16-doubly_linked_lists/lists.h
@@ -28,4 +28,5 @@ void free_dlistint(dlistint_t *head);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_idx(dlistint_t **h, unsigned int idx, int n);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 #endif

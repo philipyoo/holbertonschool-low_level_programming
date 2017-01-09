643be430cc007fcef4dc7a43f45e5280217741da
philipyoo <philipyoo10@gmail.com>
2017-01-09 20:20:18 +0000

    Edit solution for 0x16-7

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -3,35 +3,48 @@
 /**
  * insert_dnodeint_at_idx - insert a new node at given position
  * @h: double pointer to head
- * @index: index to insert into
+ * @idx: index to insert into
  * @n: value to store in new node
  * Return: Address of new node, or NULL if failed
  */
-dlistint_t *insert_dnodeint_at_idx(dlistint_t **h, unsigned int index, int n)
+dlistint_t *insert_dnodeint_at_idx(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int c;
-	dlistint_t *tmp, *hold, *new;
+	dlistint_t *tmp, *prev, *new;
 
 	tmp = *h;
-	for (c = 0; tmp && c < idx - 1; c++)
-		tmp = tmp->next;
-	if (tmp == NULL || c < idx - 1)
-		return (NULL);
-
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
-	hold = tmp->next;
 	new->n = n;
-	new->prev = tmp;
-	tmp->next = new;
-	if (hold == NULL)
-		new->next = NULL;
-	else
+	for (c = 1; tmp && c < idx; c++, tmp = tmp->next)
+		prev = tmp;
+	if (idx == 0)
 	{
-		new->next = hold;
-		hold->prev = new;
+		*h = new;
+		new->prev = NULL;
+		new->next = (tmp == NULL) ? NULL : tmp;
+		return (new);
 	}
+	if (idx == c && tmp == NULL)
+	{
+		if (prev != NULL)
+		{
+			new->prev = prev;
+			new->next = NULL;
+			prev->next = new;
+			return (new);
+		}
+		return (NULL);
+	}
+	else if (idx != c && tmp == NULL)
+		return (NULL);
+	prev = tmp;
+	tmp = tmp->next;
+	new->prev = prev;
+	new->next = tmp;
+	prev->next = new;
+	tmp->prev = new;
 
 	return (new);
 }

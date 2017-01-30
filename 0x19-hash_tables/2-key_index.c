53d1f3871c36f3592fe2ce526d6ed64e45457b32
philipyoo <philipyoo10@gmail.com>
2017-01-30 05:53:51 +0000

    Add solution for 0x19-2

---
+++ 0x19-hash_tables/2-key_index.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - Gives the index of a key
+ * @key: key of key/value pair
+ * @size: size of array of the hash table
+ * Return: index at which key/value pair should be stored in array of hash table
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key)% size);
+}
--- 0x19-hash_tables/hash_tables.h
+++ 0x19-hash_tables/hash_tables.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 /**
  * struct hash_node_s - nodes representing each key value pair within hash table
@@ -32,5 +33,6 @@ typedef struct hash_table_s
 
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
 
 #endif

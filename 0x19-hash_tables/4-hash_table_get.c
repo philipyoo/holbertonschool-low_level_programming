626c28a6a82615ec360bcb698f2f672e75d865e2
philipyoo <philipyoo10@gmail.com>
2017-02-01 03:40:26 +0000

    Add doc for 0x19-4

--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -1,5 +1,11 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_get - Retrieve a value associated with a key
+ * @ht: hash table
+ * @key: key to use to retrieve value
+ * Return: value associated with key or NULL if key not found
+ */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	hash_node_t *tmp;

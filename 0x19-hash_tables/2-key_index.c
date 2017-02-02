51cc6d30ccc9fd38667ec99d373f8f262fedd861
philipyoo <philipyoo10@gmail.com>
2017-02-02 03:13:02 +0000

    Edit solution for 0x19-2

--- 0x19-hash_tables/2-key_index.c
+++ 0x19-hash_tables/2-key_index.c
@@ -4,11 +4,9 @@
  * key_index - Gives the index of a key
  * @key: key of key/value pair
  * @size: size of array of the hash table
- * Return: index at which key/value pair should be stored in array of hash table
+ * Return: index where key/value pair should be stored in array of hash table
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	if (size == 0)
-		return (0);
-	return (hash_djb2(key)% size);
+	return (hash_djb2(key) % size);
 }

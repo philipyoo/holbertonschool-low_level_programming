4a1594f0ef41bc3c842cb6baa71107bc58c1b46e
philipyoo <philipyoo10@gmail.com>
2017-02-02 02:50:31 +0000

    Edit solution for 0x19-0

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -9,7 +9,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hsh;
 	hash_node_t **a;
-	int i;
+	unsigned long int i;
 
 	hsh = malloc(sizeof(hash_table_t));
 	if (hsh == NULL || size == 0)

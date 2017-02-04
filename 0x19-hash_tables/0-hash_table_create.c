7ae78f25d1ffeccd1ccba4a136518e866e973a8d
philipyoo <philipyoo10@gmail.com>
2017-02-04 23:44:30 +0000

    Refactor solution for 0x19-0

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -10,13 +10,19 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *hsh;
 	unsigned long int i;
 
-	hsh = malloc(sizeof(hash_table_t));
-	if (hsh == NULL || size == 0)
-		return (NULL);
-	hsh->array = malloc(sizeof(hash_node_t *) * size);
-	if (hsh->array == NULL)
+	if (size == 0)
 		return (NULL);
 
+	hsh = malloc(sizeof(hash_table_t));
+	if (hsh == NULL)
+		return (NULL);
+
+	hsh->array = malloc(sizeof(hash_node_t *) * size);
+	if (hsh->array == NULL)
+	{
+		free(hsh);
+		return (NULL);
+	}
 	hsh->size = size;
 
 	for (i = 0; i < size; i++)

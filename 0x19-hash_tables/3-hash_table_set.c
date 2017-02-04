0d5e45f002ed5fb30937161f54034a6b6dea3912
philipyoo <philipyoo10@gmail.com>
2017-02-04 23:57:13 +0000

    Edit solution for 0x19-3

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -13,12 +13,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int idx;
 
 	idx = key_index((const unsigned char *)key, ht->size);
-	if (ht->array[idx] != NULL)
+	tmp = ht->array[idx];
+
+	if (tmp != NULL)
 	{
-		tmp = ht->array[idx];
 		while (tmp)
 		{
-			if (strcmp(tmp->key, key))
+			if (strcmp(tmp->key, key) == 0)
 			{
 				tmp->key = (char *)key;
 				break;

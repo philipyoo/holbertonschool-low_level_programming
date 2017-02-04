da5e5f124eb576e1e376646e49b491e780bc791d
philipyoo <philipyoo10@gmail.com>
2017-02-04 23:07:25 +0000

    Edit solution for 0x19-4

--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -18,7 +18,10 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 		return (NULL);
 
 	while (strcmp(tmp->key, key) != 0)
-		tmp = tmp->next
+		tmp = tmp->next;
+
+	if (tmp == NULL)
+		return (NULL);
 
 	return (tmp->value);
 }

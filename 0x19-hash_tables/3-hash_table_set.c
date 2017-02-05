e200e160fc52b8c6c07a1a539115b896abed4f18
philipyoo <philipyoo10@gmail.com>
2017-02-05 00:07:53 +0000

    Edit solution for 0x19-3

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -12,6 +12,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *newpair, *tmp;
 	unsigned long int idx;
 
+	if (ht == NULL || key == NULL || value == NULL)
+		return (0);
+
 	idx = key_index((const unsigned char *)key, ht->size);
 	tmp = ht->array[idx];
 

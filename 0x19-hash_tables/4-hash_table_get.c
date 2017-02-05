23717254df6b8624dcd1dc17c4c6acfdfdbc2d83
philipyoo <philipyoo10@gmail.com>
2017-02-05 00:09:07 +0000

    Edit solution for 0x19-4

--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -11,6 +11,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *tmp;
 	unsigned long int idx;
 
+	if (ht == NULL || key == NULL)
+		return (NULL);
+
 	idx = key_index((const unsigned char *)key, ht->size);
 	tmp = ht->array[idx];
 
@@ -18,7 +21,11 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 		return (NULL);
 
 	while (strcmp(tmp->key, key) != 0)
+	{
 		tmp = tmp->next;
+		printf("tmp: %s\n", tmp->key);
+
+	}
 
 	if (tmp == NULL)
 		return (NULL);

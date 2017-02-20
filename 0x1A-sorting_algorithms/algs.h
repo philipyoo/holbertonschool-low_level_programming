dcfff43c09d39982183e596d0431806f71d9226f
philipyoo <philipyoo10@gmail.com>
2017-02-20 04:11:35 +0000

    Add header file with node struct

---
+++ 0x1A-sorting_algorithms/algs.h
@@ -0,0 +1,18 @@
+#ifndef ALGS_H
+#define ALGS_H
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+#endif

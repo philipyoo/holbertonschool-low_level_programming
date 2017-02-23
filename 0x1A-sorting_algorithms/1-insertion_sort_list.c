52ff4f159b310a6ec4ec0b8808d960fd5332ff21
philipyoo <philipyoo10@gmail.com>
2017-02-23 01:35:39 +0000

    Move helper function in insertion sort file

--- 0x1A-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1A-sorting_algorithms/1-insertion_sort_list.c
@@ -1,6 +1,19 @@
 #include "sort.h"
 
 /**
+ * get_head - Get the head of a doubly linked list
+ * @tmp: node in linked list
+ * Return: head of linked list
+ */
+listint_t *get_head(listint_t *tmp)
+{
+	while (tmp->prev)
+		tmp = tmp->prev;
+
+	return (tmp);
+}
+
+/**
  * insertion_sort_list - Sort a doubly linked list of integers
  * in ascending order using insertion sort algorithm.
  * @list: doubly linked list

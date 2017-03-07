704334903b2a3bee154332f2f487df00311c59e0
Walton Lee <mr.waltonlee@me.com>
2017-03-07 00:32:41 +0000

    null checking for malloc

--- 0x1C-binary_trees/0-binary_tree_node.c
+++ 0x1C-binary_trees/0-binary_tree_node.c
@@ -10,6 +10,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *node;
 
 	node = malloc(sizeof(binary_tree_t));
+	if (node == NULL)
+		return (NULL);
 	node->parent = parent;
 	node->n = value;
 	node->left = NULL;

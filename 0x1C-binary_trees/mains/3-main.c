c26d18f7b696b8b00f6a7df988b2eb4f5503ff23
philipyoo <philipyoo10@gmail.com>
2017-03-07 01:45:16 +0000

    Modify main file for 3

--- 0x1C-binary_trees/mains/3-main.c
+++ 0x1C-binary_trees/mains/3-main.c
@@ -14,7 +14,7 @@ int main(void)
 	root = binary_tree_node(NULL, 98);
 	root->left = binary_tree_node(root, 12);
 	root->right = binary_tree_node(root, 402);
-	binary_tree_insert_left(root->left, 128);
+	binary_tree_insert_left(root->right, 128);
 	binary_tree_insert_left(root, 54);
 	binary_tree_print(root);
 	binary_tree_delete(root);

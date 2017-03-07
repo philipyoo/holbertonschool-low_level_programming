0c6f1cdd167b01aeab43ff9d392628bb2cb4cf8e
philipyoo <philipyoo10@gmail.com>
2017-03-07 00:46:38 +0000

    Add solution for binary tree problem 1

---
+++ 0x1C-binary_trees/1-binary_tree_insert_left.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - Insert a node as the left-child of another node.
+ * If parent already has a left-child, new node must take its place, and
+ * the old left child must be set as the left-child of the new node.
+ * @parent: pointer to node to insert the left-child in
+ * @value: value to store in new node
+ * Return: pointer to the created node or NULL on failure
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->right = NULL;
+
+	if (parent->left != NULL)
+		new_node->left = tmp;
+	else
+		new_node->left = NULL;
+
+	return (new_node);
+}
--- 0x1C-binary_trees/binary_trees.h
+++ 0x1C-binary_trees/binary_trees.h
@@ -28,5 +28,5 @@ static size_t _height(const binary_tree_t *tree);
 void binary_tree_print(const binary_tree_t *tree);
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 #endif

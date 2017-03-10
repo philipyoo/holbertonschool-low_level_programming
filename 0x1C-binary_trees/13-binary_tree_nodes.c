d54cb7904f87dacb69deee2fc8c4ad173b261e29
Walton Lee <mr.waltonlee@me.com>
2017-03-10 04:13:34 +0000

    removing extra code

--- 0x1C-binary_trees/13-binary_tree_nodes.c
+++ 0x1C-binary_trees/13-binary_tree_nodes.c
@@ -6,8 +6,6 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
 	if (tree != NULL && (tree->left != NULL || tree->right != NULL))
 		return (1 + binary_tree_nodes(tree->left) +
 			binary_tree_nodes(tree->right));

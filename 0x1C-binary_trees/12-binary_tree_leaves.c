0b05b60a9360abae4936da129b2b0d4830c72ffa
Walton Lee <mr.waltonlee@me.com>
2017-03-10 01:56:21 +0000

    Counts the leaves in a binary tree

--- 0x1C-binary_trees/12-binary_tree_leaves.c
+++ 0x1C-binary_trees/12-binary_tree_leaves.c
@@ -1,13 +1,14 @@
 #include "binary_trees.h"
 /**
- *
- *
- *
+ *binary_tree_leaves - Function that counts how many leaves are in the tree
+ *@tree: The tree to be evaluated
+ *Return: The addition of both sides of the tree
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	size_t left, right;
 
+	left = right = 0;
 	if (tree == NULL)
 		return (0);
 	if (tree->left == NULL && tree->right == NULL)
---
+++ 0x1C-binary_trees/mains/12-main.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "../binary_trees.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	binary_tree_t *root;
+	size_t leaves;
+
+	root = binary_tree_node(NULL, 98);
+	root->left = binary_tree_node(root, 12);
+	root->right = binary_tree_node(root, 402);
+	binary_tree_insert_right(root->left, 54);
+	binary_tree_insert_right(root, 128);
+	binary_tree_print(root);
+
+	leaves = binary_tree_leaves(root);
+	printf("Leaves in %d: %lu\n", root->n, leaves);
+	leaves = binary_tree_leaves(root->right);
+	printf("Leaves in %d: %lu\n", root->right->n, leaves);
+	leaves = binary_tree_leaves(root->left->right);
+	printf("Leaves in %d: %lu\n", root->left->right->n, leaves);
+	return (0);
+}

eb0ebcae211a5af72f1b2de10d64986a52d91ef7
philipyoo <philipyoo10@gmail.com>
2017-03-25 22:11:00 +0000

    Edit solution for checking if node is leaf to comply with betty checks

--- 0x1C-binary_trees/4-binary_tree_is_leaf.c
+++ 0x1C-binary_trees/4-binary_tree_is_leaf.c
@@ -8,9 +8,9 @@
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL)
-		return 0;
+		return (0);
 	if (node->left == NULL && node->right == NULL)
-		return 1;
+		return (1);
 
 	return 0;
 }

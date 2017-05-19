b39505f752deba8a0001982b8ac2d0281cb69a1e
philipyoo <philipyoo10@gmail.com>
2017-05-19 05:01:33 +0000

    Edit header file, add stdlib inclusion only in header file

--- 0x1E-huffman_rb_trees/heap/heap.h
+++ 0x1E-huffman_rb_trees/heap/heap.h
@@ -1,6 +1,8 @@
 #ifndef _HEAP_H_
 #define _HEAP_H_
 
+#include <stdlib.h>
+
 /**
  * struct binary_tree_node_s - Binary tree node data structure
  *
@@ -31,13 +33,19 @@ typedef struct heap_s
 	binary_tree_node_t *root;
 } heap_t;
 
-/* helpers */
+/* general helper methods */
 size_t get_power(heap_t *heap);
+
+/* helper methods for insert */
 binary_tree_node_t *get_parent_node(heap_t *heap, size_t power);
 void min_swap_nodes(heap_t *heap, binary_tree_node_t *node);
+
+/* helper methods for extract */
+binary_tree_node_t *get_last_node(heap_t *heap, size_t power);
 
 /* functions */
 heap_t *heap_create(int (*data_cmp)(void *, void *));
 binary_tree_node_t *binary_tree_node(binary_tree_node_t *parent, void *data);
 binary_tree_node_t *heap_insert(heap_t *heap, void *data);
+void *heap_extract(heap_t *heap);
 #endif
